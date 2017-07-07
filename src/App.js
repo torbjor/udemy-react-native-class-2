d679b14d4541d8ca48367b0ae1e1a7a4503ac650
Tor Schandall <tor@schandall.net>
2017-07-06 22:21:03 -0400

    Adding firebase step

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB53Io8P2oqGhC0vO86uoA3pn9ZaUqRr8E',
+      authDomain: 'authentication-16aef.firebaseapp.com',
+      databaseURL: 'https://authentication-16aef.firebaseio.com',
+      projectId: 'authentication-16aef',
+      storageBucket: 'authentication-16aef.appspot.com',
+      messagingSenderId: '553647110242'
+    })
+  }
+
   render() {
     return (
       <View>
+        <Header headerText={'Authentication'} />
         <Text>HI</Text>
       </View>
     );
